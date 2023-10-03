82284bc25f7a00e82d38010a27d5afc4ab8ef4dd
davidcrc <david_drcr@hotmail.com>
2023-10-03 10:25:22 -0500

    feat: :fire: initial project and basic pages

---
+++ INSTALL.md
@@ -0,0 +1,11 @@
+# Setup
+
+```bash
+yarn create next-app
+```
+
+# Add Zustand
+
+```bash
+yarn add zustand
+```
---
+++ src/stores/generalStore.ts
@@ -0,0 +1,9 @@
+export interface GeneralState {
+  isLoginOpen: boolean;
+  isEditProfileOpen: boolean;
+  selectedPosts: null;
+  ids: null;
+  isBackUrl: "/";
+  posts: null;
+  suggested: null;
+}
